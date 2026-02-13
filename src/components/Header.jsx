2dcb82b904bcf620a7a8d4e149f3b036af1ec300
Olena Surilova <elen21surilova@gmail.com>
2026-02-13 22:05:58 +0100

    fix: change font-size for H1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-export default function Header () {
+export default function Header() {
   return (
     <header>
-      <hr className="border border-gray-300"/>
-      <h1 className="font-[Inter] xl:text-[238px] font-bold xl:leading-73.75 md:text-[160px] md:leading-48.75 text-[72px] leading-22">THE BLOG</h1>
-      <hr className="border border-gray-300 mb-10"/>
+      <hr className="border border-gray-300" />
+      <h1 className="font-[Inter] xl:text-[200px] font-bold xl:leading-73.75 md:text-[132px] md:leading-48.75 text-[66px] leading-22">THE BLOG</h1>
+      <hr className="border border-gray-300 mb-10" />
     </header>
   )
 }
