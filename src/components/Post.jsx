67b6a81eb93d59f78773a1b1a4668896e3d60a70
Olena Surilova <elen21surilova@gmail.com>
2026-07-28 16:36:48 +0200

    feat: show created date and edited date in Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
+  const isEdited =
+    post.updatedAt &&
+    (!post.createdAt || post.createdAt !== post.updatedAt);
+
+  console.log(isEdited, post.createdAt, '8');
+  console.log(post._id, post.updatedAt, '9');
+
+  const dateToShow = isEdited
+    ? post.updatedAt
+    : post.createdAt || post.updatedAt;
+
   return <div className="post-container flex gap-6">
     <div className="post-image-container">
       {post.imageURL ? <img src={post.imageURL} alt={post.title} /> : null}
@@ -12,6 +23,20 @@ export default function Post({ post }) {
       </h3>
       <h4 className="flex">
         {post.userId?.userName}</h4>
+
+      {dateToShow && (
+        <span className="post-date">
+          {isEdited ? 'Edited: ' : 'Published: '}
+          {new Date(dateToShow).toLocaleString('uk-UA', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+          })}
+        </span>
+      )}
+
       <p>{post.content}</p>
     </div>
   </div>
